d0c4469d10c12a5b9c0e6a3ec131b951b9707b53
Djedjiga SALMI <djedjiga_salmi@yahoo.com>
2020-12-03 15:21:23 +0100

    FOVmethod

---
+++ fovmethod/Assets/Scenes/BasicCam.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCam : MonoBehaviour
+{
+    public float maxCamDistance = 10f;
+    public float minCamFOV = 5f;
+    public float fovSpeed = 1f;
+
+    public Transform target;
+    public Camera myCam;
+
+    float initialFOV;
+
+    void Start()
+    {
+        myCam = this.GetComponent<Camera>();
+        initialFOV = myCam.fieldOfView;
+    }
+    
+    void ResetFOV()
+    {
+        myCam.fieldOfView = initialFOV;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            ResetFOV();
+        } else{
+            myCam.transform.LookAt(target);
+            if(Vector3.Distance(transform.position, target.position) > maxCamDistance){
+                if(myCam.fieldOfView<= minCamFOV){
+                    myCam.fieldOfView = minCamFOV;
+                }else{
+                    myCam.fieldOfView -= fovSpeed;
+                }
+
+            }else if (Vector3.Distance(transform.position, target.position) < maxCamDistance){
+                myCam.fieldOfView += fovSpeed;
+                if(myCam.fieldOfView >= initialFOV){
+                    ResetFOV();
+
+                }
+            }
+        }   
+    }
+}
